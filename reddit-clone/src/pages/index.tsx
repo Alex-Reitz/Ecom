0da9e4c1bd44b36a3a0fda588747a8cdf713de33
Alex Reitz <alex.j.reitz@gmail.com>
2022-01-15 16:26:48 -0500

    graphql, navbar, cache updates, logout, ssr

--- reddit-clone/src/pages/index.tsx
+++ reddit-clone/src/pages/index.tsx
@@ -1,7 +1,20 @@
-const Index = () => (
-  <div>
-    <h1>Hello World</h1>
-  </div>
-);
+import { withUrqlClient } from "next-urql";
+import { Navbar } from "../components/Navbar";
+import { usePostsQuery } from "../generated/graphql";
+import { createUrqlClient } from "../utils/createUrqlClient";
 
-export default Index;
+const Index = () => {
+  const [{ data }] = usePostsQuery();
+  return (
+    <div>
+      <Navbar />
+      {!data ? (
+        <div>Loading</div>
+      ) : (
+        data.posts.map((p) => <div key={p.id}>{p.title}</div>)
+      )}
+    </div>
+  );
+};
+
+export default withUrqlClient(createUrqlClient, { ssr: true })(Index);
--- reddit-clone/src/pages/login.tsx
+++ reddit-clone/src/pages/login.tsx
@@ -5,25 +5,25 @@ import { useRouter } from "next/router";
 import React from "react";
 import { InputField } from "../components/InputField";
 import { Wrapper } from "../components/Wrapper";
-import { useRegisterMutation } from "../generated/graphql";
+import { useLoginMutation } from "../generated/graphql";
 import { createUrqlClient } from "../utils/createUrqlClient";
 import { toErrorMap } from "../utils/toErrorMap";
 
 interface registerProps {}
 
-const Register: React.FC<registerProps> = ({}) => {
+const Login: React.FC<{}> = ({}) => {
   const router = useRouter();
-  const [, register] = useRegisterMutation();
+  const [, login] = useLoginMutation();
   return (
     <Wrapper variant="small">
       <Formik
         initialValues={{ username: "", password: "" }}
         onSubmit={async (values, { setErrors }) => {
-          const response = await register(values);
+          const response = await login({ options: values });
           console.log(response);
-          if (response.data?.register.errors) {
-            setErrors(toErrorMap(response.data.register.errors));
-          } else if (response.data?.register.user) {
+          if (response.data?.login.errors) {
+            setErrors(toErrorMap(response.data.login.errors));
+          } else if (response.data?.login.user) {
             //worked
             router.push("/");
           }
@@ -50,7 +50,7 @@ const Register: React.FC<registerProps> = ({}) => {
               isLoading={isSubmitting}
               type="submit"
             >
-              Register
+              Login
             </Button>
           </Form>
         )}
@@ -59,4 +59,4 @@ const Register: React.FC<registerProps> = ({}) => {
   );
 };
 
-export default withUrqlClient(createUrqlClient)(Register);
+export default withUrqlClient(createUrqlClient)(Login);
